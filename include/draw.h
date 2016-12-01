1678dbf62e826d48cf30bba37c7b05c6e29ddc0f
Seungok <seungok@naver.com>
2016-12-01 13:33:28 +0900

    add draw.h

---
+++ include/draw.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+ 
+
+extern int width;
+
+extern int heigh ;
+
+ 
+
+extern int table[22][27];
+
+extern int ntable[22][27] ;
+
+ 
+
+extern int diff;
+
+void draw(int diff);
