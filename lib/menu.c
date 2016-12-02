f555dfdbe1c10eb829b68b1bd18ba9b69f3b493f
Taejunhyeok <jeokjeok2@naver.com>
2016-12-01 21:24:32 -0800

    adjust menu.c

--- lib/menu.c
+++ lib/menu.c
@@ -1,26 +1,5 @@
 #include "menu.h"
 
-int getch(void)
-{
-    int ch;
-
-    struct termios old;
-    struct termios new;
-
-    tcgetattr(0, &old);
-
-    new = old;
-    new.c_lflag &= ~(ICANON|ECHO);
-    new.c_cc[VMIN] = 1;
-    new.c_cc[VTIME] = 0;
-
-    tcsetattr(0, TCSAFLUSH, &new);
-    ch = getchar();
-    tcsetattr(0, TCSAFLUSH, &old);
-
-    return ch;
-}
-
 int menu()
 {
  int check = 0;
