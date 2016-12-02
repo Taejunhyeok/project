18f9e2ed723e1f01942133590916a68f46e2938f
Taejunhyeok <jeokjeok2@naver.com>
2016-12-01 20:53:48 -0800

    add reset.h

---
+++ include/reset.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+extern int width ;
+extern int heigh ;
+extern int diff ;
+extern int table[22][27] ;
+extern int ntable[22][27] ;
+ 
+void reset(int diff);
